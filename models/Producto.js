7c50602c172785e72860cf0ecc46244265d576ff
Franklin Gutierrez <54650261+Franklp25@users.noreply.github.com>
2023-04-16 15:28:32 -0600

    Update Producto.js

--- models/Producto.js
+++ models/Producto.js
@@ -18,10 +18,10 @@ const productSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
-    categoria: {
-      type: String,
-      required: true,
-    },
+    // categoria: {
+    //   type: String,
+    //   required: true,
+    // },
   },
   {
     timestamps: true,
