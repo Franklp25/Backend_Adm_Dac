d8aef0af061cf41d88fb006e770e5bbb71d59375
XR611 <105768293+XR611@users.noreply.github.com>
2023-05-02 14:39:42 -0600

    pp

--- index.js
+++ index.js
@@ -20,19 +20,20 @@ connectDB(); // conectar a mongoDB
 //configurar CORS
 const whitelist = [process.env.FRONTEND_URL];
 
-const corsOptions = {
-    origin: function (origin, callback) {
-        // console.log(origin);
-        if (whitelist.includes(origin)) {
-            //puede consultar la API
-            callback(null, true);
-        } else {
-            //no esta permitido el  request
-            callback(new Error("Error de Cors"));
-        }
-    },
-};
-app.use(cors(corsOptions));
+// const corsOptions = {
+//     origin: function (origin, callback) {
+//         // console.log(origin);
+//         if (whitelist.includes(origin)) {
+//             //puede consultar la API
+//             callback(null, true);
+//         } else {
+//             //no esta permitido el  request
+//             callback(new Error("Error de Cors"));
+//         }
+//     },
+// };
+// app.use(cors(corsOptions));
+app.use(cors());
 
 //Routing----------------------------------------------------------------------------------------
 app.use("/api/usuarios", usuarioRoutes); //USE responde a todos los verbos http // req = Datos enviados y res= resppuesta que se obtiene
